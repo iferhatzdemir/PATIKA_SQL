3057d02defe19ef2712ffb656a51b6d67b5c4ee2
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-09-01 21:43:41 +0300

    Odev3 yapıldı

---
+++ ODEV_3/odev_3.sql
@@ -0,0 +1,4 @@
+SELECT * FROM country WHERE country LIKE 'A%a';
+SELECT * FROM country WHERE country LIKE '%______n';
+SELECT title FROM film WHERE title ~~* '%t%t%t%t%';
+SELECT * FROM film WHERE title ~~ 'C%' AND length>90 AND rental_rate = 2.99;
